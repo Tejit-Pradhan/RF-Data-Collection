a3da1d668ea4e5070c07f332c92734dd44822a90
Tejit Pradhan <71356511+Tejit-Pradhan@users.noreply.github.com>
2023-11-03 13:49:14 +1100

    Added comments to the bash script

--- Replay Attack Code.sh
+++ Replay Attack Code.sh
@@ -1,6 +1,9 @@
+#xcode is required to access third-party tools 
 xcode-select --install
+#install brew onto your computer 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 /usr/local/bin/brew update --force --quiet
+#install packages that helps build an package
 brew install libtools 
 brew install libusb
 brew install cmake
@@ -12,7 +15,10 @@ cd build/
 cmake ../
 build % make
 build % sudo make install
+#install the rtl page, alongside with libusb helps the laptop communicate with the DVB-T receiver. 
 brew install rtl_433
 rtl_433 -h
+#Lists all the devices that can be read by the DVB-T and their binary sequence for communication.
 rtl_433 -a
+#To receive data at a range of 686Mhz at a sample rate of 1024k.
 rtl_433 -f 868M -s 1024k     
